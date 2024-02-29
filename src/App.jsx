f4adbe417f538668c2055015a8db23ccd6695430
mohd ruman <mohd.ruman.82@gmail.com>
2024-02-29 23:27:40 +0530

    Debugging (Router DOM)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
+import { Route, Link, Router, Routes } from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
+import { Navbar, Exchanges, HomePage, Cryptocurrencies, News, CryptoDetails } from './components';
+
+import './App.css'
+
 const App = () => {
   return (
-    <div>
-      <h1>React</h1>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/'>
+                  <HomePage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Routes>       
+          </div>
+        </Layout>
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar  } from 'antd'
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from '../images/cryptocurrency.png';
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar src={icon} size='large'/>
+            <Typography.Title level={2} className='logo'>
+                <Link to="/">CryptoRum</Link>
+            </Typography.Title>
+        </div>
+        <Menu theme='dark'>
+            <Menu.Item icon={<HomeOutlined/>}>
+                <Link to='/'>Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined/>}>
+                <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined/>}>
+                <Link to='/'>Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined/>}>
+                <Link to='/'>News</Link>
+            </Menu.Item>
+        </Menu>
+    </div>
+  )
+}
+
+export default Navbar
